9ccdcca0353aabc5515452f091d58961820450f8
you2_kamata <diohound@gmail.com>
2021-04-15 19:28:17 +0900

    一覧取得用APIの実装開始

--- api/get_all_meeting_list.php
+++ api/get_all_meeting_list.php
@@ -1,6 +1,22 @@
 <?php
-require_once(dirname(__FILE__)."/../../env/zoom_user_setting.php");
+require_once(dirname(__FILE__)."/../../env/connection_setting.php");
 
+try{
+  $mysqli = new mysqli($SQL_HOST, $SQL_USER, $SQL_PASS, $SQL_DB);
+  $stmt = $mysqli->prepare("SELECT * FROM zoom_url");
+  $stmt->execute();
+  $list = [];
+  while($data = $stmt->fetch_assoc()){
+    $list[] = $data;
+  }
 
-
+  $result["result"] = 1;
+  $result["ins"] = $list;
+  $stmt->close();
+  $mysqli->close();
+}catch(Exception $e){
+  $result["result"] = -1;
+  $result["message"] = $e->getMessage();
+}
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
